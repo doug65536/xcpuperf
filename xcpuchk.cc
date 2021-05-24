ae037a298802a8bc34a1b6a8f6feffb69d969a8c
Doug Gale <doug16k@gmail.com>
2021-05-24 15:57:46 -0400

    Auto calibrate iteration count and get best of 100

--- xcpuchk.cc
+++ xcpuchk.cc
@@ -9,6 +9,9 @@ std::atomic_uint even_ready;
 std::atomic_uint go;
 std::atomic_uint x;
 
+using test_duration = std::chrono::high_resolution_clock::duration;
+using test_clock = std::chrono::high_resolution_clock;
+
 unsigned done_x = 3000000;
 
 template<std::memory_order load_order, std::memory_order store_order>
@@ -51,46 +54,73 @@ void test(unsigned cpus)
     for (unsigned c1 = 0; c1 < cpus; ++c1)  {
         for (unsigned c2 = 0; c2 < cpus; ++c2)  {
             if (c1 != c2) {
-                odd_ready.store(0, store_order);
-                even_ready.store(0, store_order);
+                test_duration best;
+                done_x = 300000;
+                best = test_duration::max();
 
-                std::thread t1(odd<load_order, store_order>);
-                std::thread t2(even<load_order, store_order>);
+                size_t iter_cnt = 100;
 
-                CPU_ZERO(&cpuset);
-                CPU_SET(c1, &cpuset);
-                pthread_setaffinity_np(t1.native_handle(),
-                                       sizeof(cpu_set_t), &cpuset);
+                for (size_t iter = 0; iter < iter_cnt; ++iter) {
+                    odd_ready.store(0, store_order);
+                    even_ready.store(0, store_order);
 
-                CPU_ZERO(&cpuset);
-                CPU_SET(c2, &cpuset);
-                pthread_setaffinity_np(t2.native_handle(),
-                                       sizeof(cpu_set_t), &cpuset);
+                    std::thread t1(odd<load_order, store_order>);
+                    std::thread t2(even<load_order, store_order>);
 
-                while (!odd_ready.load(load_order));
-                while (!even_ready.load(load_order));
-                x.store(0, store_order);
-                auto st = std::chrono::high_resolution_clock::now();
+                    CPU_ZERO(&cpuset);
+                    CPU_SET(c1, &cpuset);
+                    pthread_setaffinity_np(t1.native_handle(),
+                                           sizeof(cpu_set_t), &cpuset);
 
-                go.store(1, store_order);
+                    CPU_ZERO(&cpuset);
+                    CPU_SET(c2, &cpuset);
+                    pthread_setaffinity_np(t2.native_handle(),
+                                           sizeof(cpu_set_t), &cpuset);
 
-                while (even_ready.load(load_order) ||
-                       odd_ready.load(load_order));
-                auto en = std::chrono::high_resolution_clock::now();
+                    while (!odd_ready.load(load_order));
+                    while (!even_ready.load(load_order));
 
-                std::chrono::high_resolution_clock::duration elap =
-                        std::chrono::duration_cast<
-                        std::chrono::nanoseconds>(en - st);
+                    x.store(0, store_order);
+                    auto st = test_clock::now();
 
-                if (c2 == 0)
-                    printf("\n%2u", c1);
+                    go.store(1, store_order);
 
-                printf(" %3lu", elap.count() / done_x);
+                    while (even_ready.load(load_order) ||
+                           odd_ready.load(load_order));
 
-                t1.join();
-                t2.join();
+                    auto en = test_clock::now();
 
-                go.store(0, store_order);
+                    test_duration elap =
+                            std::chrono::duration_cast<
+                            std::chrono::nanoseconds>(en - st);
+
+                    uint64_t c = elap.count();
+                    bool was_better = c < 1000000;
+
+                    unsigned new_done_x = (__int128)done_x * 1000000 / c;
+
+                    if (new_done_x < done_x) {
+                        // Recalibrate
+                        done_x = new_done_x;
+
+                        // Start over
+                        iter = -1;
+                    } else if (iter > 0 && best > elap) {
+                        best = elap;
+                    }
+
+                    if (iter == (iter_cnt - 1)) {
+                        if (c2 == 0)
+                            printf("\n%2u", c1);
+
+                        printf(" %3lu", best.count() / done_x);
+                    }
+
+                    t1.join();
+                    t2.join();
+
+                    go.store(0, store_order);
+                }
             } else {
                 if (c2 == 0)
                     printf("\n%2u", c1);
@@ -112,6 +142,7 @@ int main()
     printf("testing acquire/release\n");
     test<std::memory_order_acquire, std::memory_order_release>(cpus);
 
-    printf("testing relaxed/relaxed\n");
-    test<std::memory_order_relaxed, std::memory_order_relaxed>(cpus);
+// same result
+//    printf("testing relaxed/relaxed\n");
+//    test<std::memory_order_relaxed, std::memory_order_relaxed>(cpus);
 }
